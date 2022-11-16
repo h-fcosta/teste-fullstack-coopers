78fed815e700a7ae0d6aa2da2c772e78d160452f
Henrique Costa <henriquefcosta19@gmail.com>
2022-11-16 01:51:53 -0300

    update server.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ dotEnv.config();
 
 const port = process.env.PORT || 3001;
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+}
+
 app.listen(port, () => {
   console.log(`Servidor escutando em http://localhost:${port}`);
 });
